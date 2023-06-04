203a932d0583b8787b47d714d2c072e70c2ac094
Meghann Hlibichuk <meghannfh@gmail.com>
2023-06-04 15:39:17 -0500

    text

--- src/components/standalone/SectionTextbox.jsx
+++ src/components/standalone/SectionTextbox.jsx
@@ -1,8 +1,15 @@
-export default function SectionTextbox({ title }){
+export default function SectionTextbox({ title, paragraphs }){
+
+    const subtitle = paragraphs.map((paragraph, idx) => {
+        return <h3 key={idx}>{paragraph.subtitle}</h3>
+    })
+
+    //we need to map through the text arr too and return each one 
 
     return(
         <div className="flex flex-col px-6 py-24 md:px-24">
             <h2 className="mt-6 font-semibold text-2xl md:text-5xl text-[#282828]">{title}</h2>
+            {subtitle}
         </div>
     )
 }

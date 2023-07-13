8484e731a8637d0e044162dcbf3842fa92f10c02
Meghann Hlibichuk <meghannfh@gmail.com>
2023-07-13 17:20:09 -0600

    media section finished???

--- src/components/socialmedia/MediaCard.jsx
+++ src/components/socialmedia/MediaCard.jsx
@@ -3,7 +3,7 @@ import YoutubeWidget from "./YoutubeWidget";
 
 export default function MediaCard({ icon, explanation, type, url }){
   return (
-      	<a href={url} className="block bg-white h-fit rounded-lg transition-transform ease-in-out hover:cursor-pointer hover:-translate-y-3 hover:-translate-x-3 p-2">
+      	<a href={url} className="block bg-white h-fit rounded-lg transition-transform ease-in-out hover:cursor-pointer hover:-translate-y-3 hover:-translate-x-3 p-3">
 			<div className="flex flex-row items-center justify-center gap-3">
       	 		{icon.length > 0 && icon.map((icon, idx) => (
       	   			<span className="text-4xl" key={idx}>{icon}</span>
--- src/components/socialmedia/PodcastWidget.jsx
+++ src/components/socialmedia/PodcastWidget.jsx
@@ -1,7 +1,7 @@
 export default function PodcastWidget(){
   return (
     <div>
-      <div className="">
+      <div className="mt-8">
         <iframe style={{"borderRadius":"12px"}} src="https://open.spotify.com/embed/show/6Ryzytf0Kf2jq4Vpz9RPWL?utm_source=generator&theme=0" width="100%" height="200" allowFullScreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy" title="soenglish podcast"></iframe>
       </div>
     </div>

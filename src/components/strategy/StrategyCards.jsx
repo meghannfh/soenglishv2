52be3dafcf3abf51fde0c5834306697d87f5bf0c
Meghann Hlibichuk <meghannfh@gmail.com>
2023-07-26 14:03:52 -0600

    image resizes and confirmed screen size changes

--- src/components/strategy/StrategyCards.jsx
+++ src/components/strategy/StrategyCards.jsx
@@ -11,7 +11,7 @@ export default function StrategyCards () {
             <h3 className="mt-12 text-2xl font-semibold text-slate-900">SO ENGLISH! 5つのStrategy</h3>
             <div className='flex flex-row items-center mt-10 w-full'>
                 <img src='https://res.cloudinary.com/dc1aiqs4p/image/upload/v1689782100/Graphic2edit_sdj4gm.webp' className='w-[18%] hidden lg:flex' alt='soichi graphic'/>
-                <ul className="w-full flex flex-col gap-y-3 justify-between md:flex-row md:flex-wrap md:gap-x-2 md:h-fit md:justify-center lg:grow">
+                <ul className="w-full flex flex-col gap-y-3 justify-between md:flex-row md:flex-wrap md:gap-x-2 lg:gap-x-5 xl:gap-x-2 md:h-fit md:justify-center lg:grow">
                     {strategies.map((strategy, idx) => (
                         <li key={idx} className="bg-white/75 p-10 flex items-center justify-center text-center font-semibold md:px-1 md:w-[130px] md:h-[170px] text-base lg:text-md ">{strategy}</li>
                     ))}

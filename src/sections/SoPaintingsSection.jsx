727ffaca63c1c5160d639d08fb2fde48fc3908e4
Meghann Hlibichuk <meghannfh@gmail.com>
2023-08-29 19:53:06 -0600

    added last painting

--- src/sections/SoPaintingsSection.jsx
+++ src/sections/SoPaintingsSection.jsx
@@ -1,9 +1,5 @@
 export default function SoPaintingsSection(){
 
-  // const paintingImages = [
-  //   'https://res.cloudinary.com/dc1aiqs4p/image/upload/v1688925009/24_gpt34g.webp'
-  // ]
-
   const paragraphs = [
     '最後にあと少しだけ',
     '<b>「アートがあると人生がもっと豊かになる」</b>ということに気がついた32歳から、マーカーと水彩の絵を始めました。これもアメリカの高校で、とにかく自分を表現するという彼らのスタンスにカルチャーショックを受けたことも大きく影響を受けています。絵でも歌でも、上手い下手関係なく表現することに価値があり、効率性の真逆の位置にある芸術的な表現がある人生は豊かだと思うのです。昔から作詞作曲もします。これらは、誰の為でもなく自分の為です。絵や音楽以外にも観劇に映画、小説や詩やミュージカルも趣味です。アートで人生を共に豊かにする仲間を増やす、というのもSOの密かな目標だったりするのです。',
@@ -18,33 +14,34 @@ export default function SoPaintingsSection(){
           <p className="mt-3 indent-5 leading-relaxed md:leading-loose text-lg font-medium text-slate-900" key={idx} dangerouslySetInnerHTML={{__html: text}} />
         ))}
 
-       <div className="mt-10">
-       <div class="parent">
-    <div class="div1"> </div>
-<div class="div2"> </div>
-<div class="div3"> </div>
-<div class="div4"> </div>
-<div class="div5"> </div>
-<div class="div6"> </div>
-<div class="div7"> </div>
-<div class="div8"> </div>
-<div class="div9"> </div>
-<div class="div10"> </div>
-<div class="div11"> </div>
-<div class="div12"> </div>
-<div class="div13"> </div>
-<div class="div14"> </div>
-<div class="div15"> </div>
-<div class="div16"> </div>
-<div class="div17"> </div>
-<div class="div18"> </div>
-<div class="div19"> </div>
-<div class="div20"> </div>
-<div class="div21"> </div>
-<div class="div22"> </div>
-<div class="div23"> </div>
-  </div>
-       </div>
+      <div className="mt-10">
+        <div className="parent">
+          <div className="div1"> </div>
+          <div className="div2"> </div>
+          <div className="div3"> </div>
+          <div className="div4"> </div>
+          <div className="div5"> </div>
+          <div className="div6"> </div>
+          <div className="div7"> </div>
+          <div className="div8"> </div>
+          <div className="div9"> </div>
+          <div className="div10"> </div>
+          <div className="div11"> </div>
+          <div className="div12"> </div>
+          <div className="div13"> </div>
+          <div className="div14"> </div>
+          <div className="div15"> </div>
+          <div className="div16"> </div>
+          <div className="div17"> </div>
+          <div className="div18"> </div>
+          <div className="div19"> </div>
+          <div className="div20"> </div>
+          <div className="div21"> </div>
+          <div className="div22"> </div>
+          <div className="div23"> </div>
+          <div className="div24"> </div>
+        </div>
+      </div>
 
       </div>
     </div>

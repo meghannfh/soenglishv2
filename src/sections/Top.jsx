6a0e9b9e01034fff9afebcfb18fc097fcbb684df
Meghann Hlibichuk <meghannfh@gmail.com>
2023-09-27 09:26:29 -0600

    editing top image

--- src/sections/Top.jsx
+++ src/sections/Top.jsx
@@ -39,18 +39,24 @@ export default function Top({isTopImageLoaded}){
 			)}
       <div id='top-image' className="font-bold h-screen md:h-screen relative overflow-hidden">
         <div className='absolute top-0 right-0 w-full h-full z-10 bg-black/25'></div>
-        <img 
-          srcset="
+        <picture className="h-full w-full flex" onLoad={handleTopImageLoad}>
+          <source media="(min-width: 1025px)" srcSet="https://res.cloudinary.com/dc1aiqs4p/image/upload/c_thumb,w_1600,g_auto/v1688493246/UpdatedTopImageSoEnglish_f8anih.webp" />
+          <source media="(min-width: 769px)" srcSet="https://res.cloudinary.com/dc1aiqs4p/image/upload/c_thumb,w_1024/v1688493246/UpdatedTopImageSoEnglish_f8anih.webp" />
+          <source media="(min-width: 426px)" srcSet="https://res.cloudinary.com/dc1aiqs4p/image/upload/c_thumb,w_768/v1688493246/UpdatedTopImageSoEnglish_f8anih.webp" />
+          <img src="https://res.cloudinary.com/dc1aiqs4p/image/upload/v1695602558/mobileBG_i65ha8.webp" className="h-full w-full object-cover" alt="yeah"/>
+        </picture>
+        {/* <img 
+          srcSet="
           https://res.cloudinary.com/dc1aiqs4p/image/upload/c_thumb,w_1600,g_auto/v1688493246/UpdatedTopImageSoEnglish_f8anih.webp 1600w, 
           https://res.cloudinary.com/dc1aiqs4p/image/upload/c_thumb,w_1024/v1688493246/UpdatedTopImageSoEnglish_f8anih.webp 1024w, 
           https://res.cloudinary.com/dc1aiqs4p/image/upload/c_thumb,w_768/v1688493246/UpdatedTopImageSoEnglish_f8anih.webp 768w,
           https://res.cloudinary.com/dc1aiqs4p/image/upload/v1695602558/mobileBG_i65ha8.webp 425w"
           alt="Soichi smiling at camera against cobbled-road backdrop" 
           className='h-full w-full object-cover'
-					onLoad={handleTopImageLoad}/>
+					onLoad={handleTopImageLoad}/> */}
       </div>
-      <h1 className='title-font w-full flex justify-center text-4xl text-white z-20 font-extrabold absolute top-28 md:top-28 md:text-6xl lg:top-[20rem] lg:text-7xl xl:text-8xl'>
-        <div className='flex flex-col gap-y-3 md:gap-y-5 lg:gap-0 lg:flex-row hidden'> 
+      <h1 className='title-font w-full flex justify-center text-4xl text-white z-20 font-extrabold absolute top-28 md:top-28 md:text-6xl lg:top-[15rem] lg:text-7xl xl:text-8xl'>
+        <div className='flex flex-col gap-y-3 md:gap-y-5 lg:gap-0 lg:flex-row'> 
           <div className='flex flex-row'>
             <span className='text-[#FF7D7D]'>明</span>
             <span className='text-[#FDC56F]'>日</span>
@@ -65,7 +71,7 @@ export default function Top({isTopImageLoaded}){
           </div>  
         </div>
       </h1>
-      <div className='absolute bottom-24 md:bottom-30 z-10 w-full flex justify-end md:pr-10 h-24 md:h-auto hidden'>
+      <div className='absolute bottom-24 md:bottom-30 z-10 w-full flex justify-end md:pr-10 h-24 md:h-auto'>
         <img 
         // srcSet="
         // https://res.cloudinary.com/dc1aiqs4p/image/upload/c_thumb,w_420/v1690301492/Hellotmrsoenglishtext_a9dyk3.webp 1440w"
